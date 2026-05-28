6e346033f5d4854aabb5df8cd6758c40eee7aba1
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2026-05-28 07:43:30 +0100

    Rebrand: RahauSub → Adildata, all URLs updated to adildata.com.ng

---
+++ easyfinder/inc/accessbility_controller.inc.php
@@ -0,0 +1,8 @@
+<?php
+/*
+if(!$site_settings->check_admin_sub_url_accessbility($Auth->admin_role,$URL_NAME) AND !$site_settings->check_admin_url_accessbility($Auth->admin_role,$URL_NAME)){
+$site_settings->redirect(SITE_URL.'easyfinder/dashboard');
+ exit();
+}
+*/
+?>
